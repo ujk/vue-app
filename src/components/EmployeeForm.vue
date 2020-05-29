0828fbdb01a16f0a33a9f5047e9740cddc09bae9
ujk <ujk7107@gmail.com>
2020-05-29 20:18:51 +0300

    initial commit

---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,98 @@
+﻿<template>
+  <div id="employee-form">
+    <form @submit.prevent="handleSubmit">
+      <label>Çalışanın Adı</label>
+      <input 
+        ref="first" 
+        v-model="employee.name" 
+        type="text" 
+        :class="{ 'has-error': submitting && invalidName }" 
+        @focus="clearStatus" 
+        @keypress="clearStatus" 
+      />
+      <label>Çalışanın Emaili</label>
+      <input 
+        v-model="employee.email" 
+        type="text" 
+        :class="{ 'has-error': submitting && invalidEmail }" 
+        @focus="clearStatus" 
+      />
+      <p v-if="error && submitting" class="error-message">
+        ❗ Lütfen tüm alanları doldurun
+      </p>
+      <p v-if="success" class="success-message">
+        ✅ Çalışan başarıyla eklendi
+      </p>
+      <button>Çalışan Ekle</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "employee-form",
+    data() {
+      return {
+        submitting: false,
+        error: false,
+        success: false,
+        employee: {
+          name: "",
+          email: ""
+        }
+      }
+    },
+    methods: {
+      handleSubmit() {
+        this.submitting = true
+        this.clearStatus()
+
+        if (this.invalidName || this.invalidEmail) {
+          this.error = true
+          return
+        }
+
+        this.$emit("add:employee", this.employee)
+        this.$refs.first.focus()
+        this.employee = {
+          name: "",
+          email: ""
+        }
+        this.error = false
+        this.success = true
+        this.submitting = false
+      },
+
+      clearStatus() {
+        this.success = false
+        this.error = false
+      }
+    },
+    computed: {
+      invalidName() {
+        return this.employee.name === ""
+      },
+      invalidEmail() {
+        return this.employee.email === ""
+      }
+    }
+  };
+</script>
+
+<style scooped>
+  form {
+    margin-bottom: 2rem;
+  }
+
+  [class*="-message"] {
+    font-weight: 500;
+  }
+
+  .error-message {
+    color: #d33c40;
+  }
+
+  .success-message {
+    color: #32a95d;
+  }
+</style>
---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,78 @@
+﻿<template>
+    <div id="employee-table">
+
+        <p v-if="employees.length < 1" class="empty-table">
+            Çalışan kaydı bulunamadı
+        </p>
+        <table v-else>
+            <thead>
+                <tr>
+                    <th>Çalışanın Adı</th>
+                    <th>Çalışanın Emaili</th>
+                    <th>İşlemler</th>
+                </tr>
+            </thead>
+            
+            <tbody>
+                <tr v-for="employee in employees" :key="employee.id">
+                    <td v-if="editing === employee.id">
+                        <input type="text" v-model="employee.name">
+                    </td>
+                    <td v-else>{{ employee.name }}</td>
+
+                    <td v-if="editing === employee.id">
+                        <input type="text" v-model="employee.email">
+                    </td>
+                    <td v-else>{{ employee.email }}</td>
+
+                    <td v-if="editing === employee.id">
+                        <button @click="editEmployee(employee)">Kaydet</button>
+                        <button class="muted-button" @click="cancelEdit(employee)">İptal</button>
+                    </td>
+                    <td v-else>
+                        <button @click="editMode(employee)">Düzenle</button>
+                        <button @click="$emit('delete:employee', employee.id)">Sil</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "employee-table",
+        props: {
+            employees: Array
+        },
+        data() {
+            return {
+                editing: null
+            }
+        },
+        methods: {
+            editMode(employee) {
+                this.cachedEmployee = Object.assign({}, employee)
+                this.editing = employee.id
+            },
+            cancelEdit(employee) {
+                Object.assign(employee, this.cachedEmployee)
+                this.editing = null
+            },
+
+            editEmployee(employee) {
+                if (employee.name === "" || employee.email === "") return
+                this.$emit("edit:employee", employee.id, employee)
+                this.editing = null
+            }
+        }
+    };
+</script>
+
+<style scooped>
+    button {
+        margin: 0 0.5rem 0 0;
+    }
+</style>
+
